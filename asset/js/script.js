69e5d98021e070209e2fe466d16b8529b4a2e397
BennyKharisma <beni.karisma@yahoo.com>
2023-05-20 17:09:29 +0700

    Add animation and adjust layout

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,3 +1,56 @@
+var words = 
+    [
+        'SE INDONESIA', 
+        'BERKUALITAS', 
+        'BERPENGALAMAN', 
+        'PROFESIONAL'
+    ],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 80;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
+
 $("#submit").click(function(){
     let nomor = '6281314088904'
     let name = document.getElementById('name').value
